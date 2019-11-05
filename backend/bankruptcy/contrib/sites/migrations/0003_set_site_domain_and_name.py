5720c56b4436ef9649c98ed190f97e65d127f7a9
Euirim Choi <euirim@me.com>
2019-11-05 09:11:24 -0800

    Update hosts

--- backend/bankruptcy/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ backend/bankruptcy/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -13,7 +13,7 @@ def update_site_forward(apps, schema_editor):
     Site.objects.update_or_create(
         id=settings.SITE_ID,
         defaults={
-            "domain": "euirim.org",
+            "domain": "52.8.169.128",
             "name": "Bankruptcy Map",
         },
     )
