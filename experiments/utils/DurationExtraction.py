a25e6b059e0febb8c27d54b5deeb55c80b50efdc
kate-wang <katewang42@gmail.com>
2019-11-04 21:57:24 -0800

    updated duration extraction code

--- experiments/utils/DurationExtraction.py
+++ experiments/utils/DurationExtraction.py
@@ -11,26 +11,12 @@ for file in os.listdir(DATA_LOCATION):
         case_files.append(os.path.join(DATA_LOCATION, file))
 
 #Returns duration of a case in days
-def FindDurationFromCase(case):
-    newest_date = ""
-    entries = case.get_entries()
-    if(len(entries) > 1):
-        oldest_date = entries[0].get_date_filed()
-        newest_date = entries[1].get_date_filed()
-        for i in range(2, len(entries)):
-            entry_date = entries[i].get_date_filed()
-            if(entry_date > newest_date):
-                newest_date = entry_date
-        duration = newest_date - oldest_date
-        return duration.days
-    elif(len(entries) == 1):
-        oldest_date = entries[0].get_date_filed()
-        newest_date = entries[0].get_date_modified()
-        duration = newest_date - oldest_date
-        return duration.days
-                         
+def FindDurationAndStartDateFromCase(case):
+    if(case.get_date_filed() != None and case.get_date_terminated().date() != datetime.datetime.utcnow().date()):
+        duration = case.get_date_terminated() - case.get_date_filed()
+        return [case.get_date_filed(), duration.days]
     #In the case no duration is found, return None
     return None
 
 #Example with a chapter 11 case:
-FindDurationFromCase(Case("../data/samples_5000/7325782.json"))
+FindDurationFromCase(Case("../data/samples/7325782.json"))
