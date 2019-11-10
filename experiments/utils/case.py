52a2dcc5b413fc6f81c17529815a5c77a96dc35c
Euirim Choi <euirim@me.com>
2019-11-10 05:41:07 -0800

    Improve pdf text extraction

--- experiments/utils/case.py
+++ experiments/utils/case.py
@@ -3,9 +3,10 @@ import os
 import urllib.request
 import shutil
 
-import PyPDF2
 import arrow
-import textract
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
 
 
 class Case:
@@ -27,22 +28,75 @@ class Case:
         return self.__data["court"]
 
     def get_date_filed(self):
-        return arrow.get(self.__data["date_filed"])
+        if self.__data["date_filed"]:
+            return arrow.get(self.__data["date_filed"]).date()
+        else:
+            return None
+        
+    def get_date_created(self):
+        if self.__data["date_created"]:
+            return arrow.get(self.__data["date_created"]).date()
+        else:
+            return None
 
     def get_date_terminated(self):
-        return arrow.get(self.__data["date_terminated"])
+        if self.__data["date_terminated"]:
+            return arrow.get(self.__data["date_created"]).date()
+        else:
+            return None
 
     def get_date_blocked(self):
-        return arrow.get(self.__data["date_blocked"])
+        if self.__data["date_blocked"]:
+            return arrow.get(self.__data["date_blocked"]).date()
+        else:
+            return None
 
     def get_pacer_id(self):
-        return self.__data["pacer_id"]
+        return self.__data["pacer_case_id"]
 
     def get_case_name(self):
         return self.__data["case_name"]
 
     def get_entries(self):
         return self.entries
+
+    def get_chapter(self):
+        """
+        In most cases, extracts a chapter number
+        purely from case metadata.
+        """
+        for entry in self.get_entries():
+            descr = entry.get_description()
+
+            if descr is not None and "petition" in descr.lower():
+                if "chapter 7" in descr.lower():
+                    return 7
+                elif "chapter 11" in descr.lower():
+                    return 11
+                elif "chapter 12" in descr.lower():
+                    return 12
+                elif "chapter 13" in descr.lower():
+                    return 13
+                elif "chapter 15" in descr.lower():
+                    return 15
+            else:
+                for doc in entry.documents:
+                    doc_descr = doc.get_description()
+                    if doc_descr is None or "petition" not in doc_descr.lower():
+                        return None
+
+                    if "chapter 7" in doc_descr.lower():
+                        return 7
+                    elif "chapter 11" in doc_descr.lower():
+                        return 11
+                    elif "chapter 12" in doc_descr.lower():
+                        return 12
+                    elif "chapter 13" in doc_descr.lower():
+                        return 13
+                    elif "chapter 15" in doc_descr.lower():
+                        return 15
+                                    
+        return None
 
 
 class DocketEntry:
@@ -57,13 +111,22 @@ class DocketEntry:
         return self.__data["id"]
 
     def get_date_filed(self):
-        return arrow.get(self.__data["date_filed"])
+        if self.__data["date_filed"]:
+            return arrow.get(self.__data["date_filed"]).date()
+        else:
+            return None
 
-    def get_date_modified(self):
-        return arrow.get(self.__data["date_modified"])
+    def get_date_created(self):
+        if self.__data["date_created"]:
+            return arrow.get(self.__data["date_created"]).date()
+        else:
+            return None
 
     def get_description(self):
-        return self.__data["description"]
+        if self.__data["description"]:
+            return self.__data["description"].strip()
+        else:
+            return None
 
     def get_documents(self):
         return self.documents
@@ -84,37 +147,81 @@ class Document:
         return self.__data["document_type"]
 
     def get_description(self):
-        return self.__data["description"]
+        if self.__data["description"]:
+            return self.__data["description"].strip()
+        else:
+            return None
+
+    def get_file_url(self):
+        return self.__data["filepath_ia"]
 
     def is_sealed(self):
-        return self.__data["is_sealed"]
+        if not self.__data["is_sealed"]:
+            return False
+        else:
+            return self.__data["is_sealed"]
 
     def is_available(self):
-        return self.__data["is_available"]
+        if not self.__data["is_available"]:
+            return False
+        else:
+            return self.__data["is_available"]
 
     def download(self):
-        url = self.__data["filepath_ia"]
-        if not self.is_available() or not url:
+        url = self.get_file_url()
+        if not self.is_available() and not url:
             return
-        directory = 'tmp'
+        directory = 'tmp/{}'.format(self.get_id())
 
         # make directory if it doesn't exist
         try:
             os.stat(directory)
         except:
-            os.mkdir(directory)
+            os.makedirs(directory, exist_ok=True)
 
-        filename = '{0}/{1}'.format(directory, self.get_id())
+        pdf_filename = '{0}/doc.pdf'.format(directory)
 
-        with urllib.request.urlopen(url) as response, open(filename, 'wb+') as out_file:
+        with urllib.request.urlopen(url) as response, open(pdf_filename, 'wb+') as out_file:
             response = response.read()
             out_file.write(response)
 
-        with open(filename, 'rb') as f:
-            pdfReader = PyPDF2.PdfFileReader(f)
-            self.text = ''
-            for page in pdfReader.pages:
-                self.text += ' ' + page.extractText() 
+        # convert PDF to images
+        page_images = convert_from_path(pdf_filename, 450)
+
+        page_filenames = []
+        # crop images
+        for i, page in enumerate(page_images):
+            page = page.crop(
+                (
+                    0, 
+                    0 + page.height * 0.055, 
+                    page.width, 
+                    page.height - page.height * 0.11,
+                )
+            )
+            fn = 'pg_{}.jpg'.format(i)
+            fn = '{0}/{1}'.format(directory, fn)
+            page.save(fn)
+            page_filenames.append(fn)
+        
+        # convert images to text and compile
+        texts = []
+        for fn in page_filenames:
+            text = str(pytesseract.image_to_string(Image.open(fn)))
+            text = text.replace('-\n', '')
+            text = text.replace('\n', ' ')
+            text = text.strip()
+            print(text)
+            texts.append(text)
+            
+        result = ' '.join(texts)
+        
+        # clean up
+        for fn in page_filenames:
+            os.remove(fn)
+        os.remove(pdf_filename)
+        
+        return result
 
 
 class Party:
