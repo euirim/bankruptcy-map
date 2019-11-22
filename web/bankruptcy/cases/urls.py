39e34f051307b24c2ea69a074cae7d65978434dc
Euirim Choi <euirim@me.com>
2019-11-22 11:03:30 -0800

    Remove web and move to another repo

--- web/bankruptcy/cases/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import search, CaseDetail
-
-app_name = "cases"
-urlpatterns = [
-    path("search/", view=search, name="search"),
-    path("<int:pk>/", view=CaseDetail.as_view(), name="detail"),
-    path("/", view=search, name="search"),
-]
